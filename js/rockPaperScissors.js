9baa753bdbc39dc5a44a49a568c2ec6486290718
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-03-31 16:57:24 +0100

    Add getComputerChoice function

---
+++ js/rockPaperScissors.js
@@ -0,0 +1,28 @@
+// Create a function for the computer's choice.
+// Computer's choice needs to be random of 3 choices:
+// Rock, Paper or Scissors.
+
+// Create random number for each move (rock, paper and scissors)
+// randomNumber generates 3 numerical choices (1, 2 and 3)
+// Assign each move to a value
+// Return the value and log to the console
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    let computerChoice;
+
+    if (randomNumber === 1) {
+        computerChoice = "Rock";
+        console.log("Rock");
+    } else if (randomNumber === 2) {
+        computerChoice = "Paper";
+        console.log("Paper");
+    } else if (randomNumber === 3) {
+        computerChoice = "Scissors";
+        console.log("Scissors");
+    }
+    return computerChoice;
+
+}
+
+getComputerChoice();
