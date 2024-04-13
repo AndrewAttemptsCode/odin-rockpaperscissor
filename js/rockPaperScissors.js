bb16392f38560cded7f219deecda9caa3089846c
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-04-13 13:09:53 +0100

    Remove comments

--- js/rockPaperScissors.js
+++ js/rockPaperScissors.js
@@ -1,33 +1,16 @@
-// Create a function for the computer's choice.
-// Computer's choice needs to be random of 3 choices:
-// Rock, Paper or Scissors.
-
-// Create random number for each move (rock, paper and scissors)
-// randomNumber generates 3 numerical choices (1, 2 and 3)
-// Assign each move to a value
-// Return the value and log to the console
-
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
     let computerChoice;
     
     if (randomNumber === 1) {
         computerChoice = "rock";
-        // console.log("The computer picked, Rock.");
     } else if (randomNumber === 2) {
         computerChoice = "paper";
-        // console.log("The computer picked, Paper.");
     } else if (randomNumber === 3) {
         computerChoice = "scissors";
-        // console.log("The computer picked, Scissors.");
     }
     return computerChoice;
 }
-
-// Function to play a single round of Rock, Paper Scissors.
-// Takes input from the variables.
-// Decides winner based on the move hierarchy.
-// Return correct round statement in the console.
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
@@ -51,17 +34,6 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-// Test the outcome of playRound
-// console.log(playRound(playerSelection, computerSelection));
-
-// Create function to play 5 rounds
-// Player select is static - place outside of the loop.
-// Player select is now dynamic, asks for a move.
-// Insert computer function into the loop to generate new move each time.
-// Insert playRound function to compare player and computer move to decide outcome.
-// Add score tracking
-// Add score reporting at the end of the 5 rounds.
-// Add flexability for rounds amount in the for loop call.
 function playGame(rounds) {
     for (let i = 0; i < rounds; i++) {
         const playerSelection = prompt("Enter a move (Rock, Paper or Scissors): ");
