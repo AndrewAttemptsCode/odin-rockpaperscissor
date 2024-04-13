8b6354af158ba277bff635a74d79956e75ccc727
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-04-13 15:42:54 +0100

    Add button function and response to clicks

--- js/rockPaperScissors.js
+++ js/rockPaperScissors.js
@@ -12,8 +12,9 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
+function playRound(playerSelection) {
+    const computerSelection = getComputerChoice();
+
 // If both selections are the same, it's a draw.
     if (playerSelection === computerSelection) {
         alert(`You picked, ${playerSelection}.\nThe computer picked, ${computerSelection}.\n\nIT'S A DRAW!`);
@@ -34,18 +35,25 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function playGame(rounds) {
-    for (let i = 0; i < rounds; i++) {
-        const playerSelection = prompt("Enter a move (Rock, Paper or Scissors): ");
-        const computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
-    }
+// function playGame(rounds) {
+//     for (let i = 0; i < rounds; i++) {
+//         //const computerSelection = getComputerChoice();
+//         playRound(playerSelection, computerSelection);
+//     }
 
-    let resultMessage = playerScore === computerScore ? "IT'S A DRAW" : playerScore > computerScore ? "YOU WIN!" : "YOU LOSE.";
-        alert(`FINAL SCORE\n\nYour score: ${playerScore}/${rounds}\nComputer score: ${computerScore}/${rounds}\n\n${resultMessage}`);
-}
+//     let resultMessage = playerScore === computerScore ? "IT'S A DRAW" : playerScore > computerScore ? "YOU WIN!" : "YOU LOSE.";
+//         alert(`FINAL SCORE\n\nYour score: ${playerScore}/${rounds}\nComputer score: ${computerScore}/${rounds}\n\n${resultMessage}`);
+// }
 
 let playerScore = 0;
 let computerScore = 0;
 
-playGame(5);
+// playGame(5);
+
+const buttonRock = document.querySelector("#rock");
+const buttonPaper = document.querySelector("#paper");
+const buttonScissors = document.querySelector("#scissors");
+
+buttonRock.addEventListener("click", () => playRound("rock"));
+buttonPaper.addEventListener("click", () => playRound("paper"));
+buttonScissors.addEventListener("click", () => playRound("scissors"));
